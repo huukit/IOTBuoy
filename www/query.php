28e2b2e59c9caac4281f3bf745505cbb5fcc7331
Tuomas Huuki <tuomas.huuki@agcocorp.com>
2017-10-18 14:47:26 +0300

    Added fullinfo-query to get latest status.

--- www/query.php
+++ www/query.php
@@ -1,89 +1,126 @@
-<!DOCTYPE html>
-<html lang = "en-US">
-    <head>
-        <meta charset = "UTF-8">
-        <title>Query</title>
-    </head>
-    <body>
-        <?php
-        $servername = "localhost";
-        $username = "poiju";
-        $password = "poiju";
-        $dbname = "Poijut";
+<?php
+    $servername = "localhost";
+    $username = "poiju";
+    $password = "poiju";
+    $dbname = "Poijut";
 
-        // Data or information
-        $info = filter_input(INPUT_GET, 'info');
+    // Data or information
+    $info = filter_input(INPUT_GET, 'info');
 
-        // ID and datatype.
-        $id = filter_input(INPUT_GET, 'id');
-        $type = filter_input(INPUT_GET, 'datatype');
+    // ID and datatype.
+    $id = filter_input(INPUT_GET, 'id');
+    $type = filter_input(INPUT_GET, 'datatype');
 
-        // Limits, etc.
-        $limit = filter_input(INPUT_GET, 'limit');
-        $sdate = filter_input(INPUT_GET, 'sdate');
-        $edate = filter_input(INPUT_GET, 'edate');
+    // Limits, etc.
+    $limit = filter_input(INPUT_GET, 'limit');
+    $sdate = filter_input(INPUT_GET, 'sdate');
+    $edate = filter_input(INPUT_GET, 'edate');
 
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Information query, basic
+    if ($info == 'info'){
+        if(empty($id)){
+            $sql = "SELECT * FROM poijut";
         }
+        else{
+            $sql = "SELECT * FROM poijut WHERE id = $id";
+        }
+        $result = $conn->query($sql);
+        $data = array();
 
-        // Information query
-        if ($info == 'true'){
-            if(empty($id)){
-                $sql = "SELECT * FROM poijut";
-            }
-            else{
-                $sql = "SELECT * FROM poijut WHERE id = $id";
-            }
-            $result = $conn->query($sql);
-            $data = array();
-
-            if ($result->num_rows > 0) {
-                 while($row = $result->fetch_assoc()) {
-                     $rowarr["id"] = $row["id"];
-                     $rowarr["name"] = $row["name"];
-                     $rowarr["position"] = $row["position"];
-                     $rowarr["date_added"] = $row["date_added"];
-                     array_push($data, $rowarr);
-                 }
-            }
-            echo json_encode($data);
+        if ($result->num_rows > 0) {
+             while($row = $result->fetch_assoc()) {
+                 $rowarr["id"] = $row["id"];
+                 $rowarr["name"] = $row["name"];
+                 $rowarr["position"] = $row["position"];
+                 $rowarr["date_added"] = $row["date_added"];
+                 array_push($data, $rowarr);
+             }
+        }
+        echo json_encode($data);
+    }
+    
+    // Information query, with last measurements.
+    elseif ($info == 'fullinfo'){
+        if(empty($id)){
+            $sql = "SELECT * FROM poijut";
+        }
+        else{
+            $sql = "SELECT * FROM poijut WHERE id = $id";
+        }
+        $result = $conn->query($sql);
+        $data = array();
+        
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $rowarr = [];
+                $rowarr["id"] = $row["id"];
+                $rowarr["name"] = $row["name"];
+                $rowarr["position"] = $row["position"];
+                $rowarr["date_added"] = $row["date_added"];
+                
+                $id = $row["id"];
+                $sql = "SELECT * FROM data WHERE id = $id ORDER BY timestamp DESC LIMIT 1";
+                $dataresult = $conn->query($sql);
+                
+                if ($result->num_rows > 0){
+                    while($row = $dataresult->fetch_assoc()) {
+                        $rowarr["timestamp"] = $row["timestamp"];
+                        $rowarr["rssi"] = $row["rssi"];
+                        $rowarr["batterymV"] = $row["batterymV"];
+                        $rowarr["airTemp"] = $row["airTemp"];
+                        $rowarr["airHumidity"] = $row["airHumidity"];
+                        $rowarr["airPressureHpa"] = $row["airPressureHpa"];
+                        $rowarr["waterArrayCount"] = $row["waterArrayCount"];
+                        $rowarr["waterTemp1"] = $row["waterTemp1"];
+                        $rowarr["waterTemp2"] = $row["waterTemp2"];
+                        $rowarr["waterTemp3"] = $row["waterTemp3"];
+                        $rowarr["waterTemp4"] = $row["waterTemp4"];
+                        $rowarr["waterTemp5"] = $row["waterTemp5"];
+                        $rowarr["waterTemp6"] = $row["waterTemp6"];
+                    }
+                }
+             array_push($data, $rowarr);
+             }
         }
         
-        // Data query
-        else{
-            $sql = "SELECT timestamp, $type FROM data WHERE id = $id";
-            
-            if(!empty($sdate)){
-                $sql = $sql . " AND timestamp >= '$sdate'";
-            }
-            
-            if(!empty($edate)){
-                $sql = $sql . " AND timestamp <= '$edate'";
-            }
-            
-            // Limit active.
-            if(!empty($limit))  {
-                $sql = $sql . " ORDER BY timestamp DESC LIMIT $limit";
-            }
+        echo json_encode($data);
+    }
+    
+    // Data query
+    else{
+        $sql = "SELECT timestamp, $type FROM data WHERE id = $id";
 
-            echo $sql;
-            $result = $conn->query($sql);
-            $data = array();
-            if ($result->num_rows > 0) {
-                $arr = array();
-                while($row = $result->fetch_assoc()) {
-                    array_push($data, array($row["timestamp"] => $row["$type"]));
-                 }
-                 echo json_encode($data);
-            } else {
-                echo "[]";
-            }
+        if(!empty($sdate)){
+            $sql = $sql . " AND timestamp >= '$sdate'";
         }
-        $conn->close();
-        ?>
-</body>
-</html>
+
+        if(!empty($edate)){
+            $sql = $sql . " AND timestamp <= '$edate'";
+        }
+
+        // Limit active.
+        if(!empty($limit))  {
+            $sql = $sql . " ORDER BY timestamp DESC LIMIT $limit";
+        }
+        
+        $result = $conn->query($sql);
+        $data = array();
+        if ($result->num_rows > 0) {
+            $arr = array();
+            while($row = $result->fetch_assoc()) {
+                array_push($data, array($row["timestamp"] => $row["$type"]));
+             }
+             echo json_encode($data);
+        } else {
+            echo "[]";
+        }
+    }
+    $conn->close();
+?>
